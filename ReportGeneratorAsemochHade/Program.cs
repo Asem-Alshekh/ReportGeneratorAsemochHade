9fb8fcd32859e9e7d23d374b2233885afe0ff696
Asem-Alshekh <asemsuha@gmail.com>
2024-10-31 00:31:09 +0100

    this is modifed

--- ReportGeneratorAsemochHade/Program.cs
+++ ReportGeneratorAsemochHade/Program.cs
@@ -2,6 +2,7 @@
 {
     public static class Program
     {
+
         public static void Main(string[] args)
         {
             var reportFactory = new ReportFactory();
@@ -15,11 +16,9 @@
             var wordReport = reportFactory.CreateReport("Word");
             wordReport.Generate();
 
-
             var asciDocReport = reportFactory.CreateReport("Ascidoc");
             asciDocReport.Generate();
-
-
+            Console.WriteLine  ();
 
             try
             {
